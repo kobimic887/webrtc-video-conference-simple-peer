39a572daa5802aa9a78e226e98b33449256a63b7
anton genis <antonge@essence-grp.com>
2021-10-09 11:07:48 +0300

    add photos

--- src/app.js
+++ src/app.js
@@ -24,6 +24,27 @@ const io = require('socket.io')(httpsServer)
 require('./socketController')(io)
 
 
+app.use('/gallery', require('node-gallery')({
+    staticFiles : 'resources/photos',
+    urlRoot : 'gallery', 
+    title : 'Girls-Chat'
+  }));
+
+ /* app.use('/gallery', require('node-gallery')({
+    staticFiles : 'resources/photos',
+    urlRoot : 'gallery', 
+    title : 'Girls-Cha',
+    render : false
+  }), function(req, res, next){
+    
+     //We MUST add another middleware function to the chain when render is false. 
+     //just return the raw HTML data - we could partial into another template here,
+     //pass the JSON data into a template
+     
+    return res.send(req.html);
+  });
+*/
+
 httpsServer.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
